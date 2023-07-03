2899d3ef577e52237533dd73daae371fb3f42f94
TortoiseDev <cutiecatdaog@gmail.com>
2023-07-03 15:01:09 +0200

    Added new download function, script can now run on any platform

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
+import random
+import string
 import requests
 from bs4 import BeautifulSoup as bs
 import os
+from tqdm import tqdm
 
 # Defines Number of Episodes
-startEp = int(input("Start from episode?: "))
-numOfEps= int(input("How many episodes?: "))
+try:
+    startEp = int(input("Start from episode?: "))
+    numOfEps= int(input("How many episodes?: "))
+except:
+    # Default values (1,1)
+    print("Error occured, defaulted values to (1,1)")
+    startEp = 1
+    numOfEps = 1
+# Range of startEp to startEp and numOfEps exclusive
 eps = list(range(startEp,startEp+numOfEps))
 # Takes link input
 print("Example : https://gogoanime.hu//k-on-2-episode")
-link = input("Enter the link: ")
+try:
+    link = input("Enter the link: ")
+    name = input("Enter anime name: ")
+    try:
+        os.mkdir(f"./{name}")
+    except:
+        print("Directory exists")
+    name = f"./{name}"
+except:
+    # Default value
+    print("Error occured, link is now default value.")
+    link = "https://gogoanime.hu//k-on-2-episode"
+    name = "Generic"
 # Takes email input
 email = input("Enter your email: ")
 # Takes password input
@@ -57,6 +79,7 @@ def LoginAndGoToLink(eps,Email,Password,LinkofPath):
     # Returns Links array to other functions
     return Links
 
+# Not used anymore
 # Downloading function, takes Links array as a parameter
 def WGetTheFiles(Links):
     name = startEp
@@ -64,6 +87,41 @@ def WGetTheFiles(Links):
         # Downloads each file
         os.system(f"wget -O EP{name}.mp4 {Link}")
         name+=1
-WGetTheFiles(LoginAndGoToLink(eps,email,password,link))
+# Generates random color code for progress bar
+def generate_hex_color_code():
+    # Generate a random 6-digit hex number
+    hex_num = ''.join(random.choices(string.hexdigits, k=6))
+    # Prepend '#' to the hex number to get a valid color code
+    color_code = '#' + hex_num
+    return color_code
+
+# Used to download the file and is multiplatform
+def DownloadTheFiles(Links,path):
+    name = startEp
+    for link in Links:
+        # Get the file in chunks
+        response = requests.get(link, stream=True)
+        # Calculate size in Bytes
+        total_size_in_bytes = int(response.headers.get('content-length', 0))
+        # Block size of progress bar
+        block_size = 1024  # 1 Kibibyte
+        # Actual progress bar
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True,colour=generate_hex_color_code(),desc=f"EP{name}.mp4 ")
+        # Opens file in binary write mode
+        with open(f"{path}/EP{name}.mp4", "wb") as file:
+            # Loops through response as it write it in chunks
+            for data in response.iter_content(block_size):
+                # Updates progress bar
+                progress_bar.update(len(data))
+                # Writes data
+                file.write(data)
+        # Closes progress bar to avoid memory leaks
+        progress_bar.close()
+        # Increments name
+        name+=1
+
+# WGetTheFiles(LoginAndGoToLink(eps,link),name)
+DownloadTheFiles(LoginAndGoToLink(eps,email,password,link),name)
+
 
 # ENJOY

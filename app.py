fc58b3466e01fa21829534c059980ad24cb78399
TortoiseDev <cutiecatdaog@gmail.com>
2023-08-02 21:32:38 +0300

    Added search so everything happens in the code

--- app.py
+++ app.py
@@ -1,48 +1,141 @@
 import random
 import string
+import time
 import requests
 from bs4 import BeautifulSoup as bs
 import os
+import datetime
 from tqdm import tqdm
 
-# Defines Number of Episodes
-try:
-    startEp = int(input("Start from episode?: "))
-    numOfEps= int(input("How many episodes?: "))
-except:
-    # Default values (1,1)
-    print("Error occured, defaulted values to (1,1)")
-    startEp = 1
-    numOfEps = 1
-# Range of startEp to startEp and numOfEps exclusive
-eps = list(range(startEp,startEp+numOfEps))
-# Takes link input
-print("Example : https://gogoanime.hu//k-on-2-episode")
-try:
-    link = input("Enter the link: ")
-    name = input("Enter anime name: ")
+# Fucntion that prints search results
+def displaySeachResults(results : dict) -> None:
+    # Loops through keys and values in the dictionary
+    for id,result in results.items():
+        # Prints ID and Name
+        print(f"The ID is {id}")
+        print(f"The name is {result['Name']}")
+        print("\n")
+
+# Search function
+def search() -> str:
+    # Loops until valid inputt is given
+    while True:
+        try:
+            search : str = input("Enter the anime you want to search for: ")
+            break
+        except:
+            print("Invlaid input, try again")
+
+    # Creates search link 
+    link : str = f"https://gogoanimehd.to/search.html?keyword={search.replace(' ','%20')}"
+
+    # Creates a new session
+    session : requests.Session = requests.session()
+    # Gets the page's source
+    htnlData = session.get(link).text
+    # Parses the data
+    soupedData = bs(htnlData, "lxml")
+    # Gets the search results container
+    container = soupedData.find("ul",{"class": "items"})
+
+    # Creates an empty dicitionary and an index variable
+    shows : dict = {}
+    i : int = 0
+    # Loops through all results
+    for show in container.find_all("li"):
+        # Gets the p tag containing the name and link
+        paragraph = show.find("p", {"class":"name"})
+        # Gets the a tag within it
+        aTag = paragraph.find("a")
+        # Gets showname and link
+        showName : str = aTag.getText()
+        showLink : str = aTag.get("href")
+        # Updates the dicitionary and increments i
+        shows.update({i: {"Name" : showName,"Link" :showLink}})
+        i+=1
+    # Displays search results
+    displaySeachResults(shows)
+    # Loops until a valid id is given
+    while True:
+        try:
+            showID : int = int(input("Enter ID of the correct result: "))
+            break
+        except:
+            print("Invalid, try again") 
+    # Returns the link
+    return shows[showID]["Link"]
+
+# Function to get the number of episodes
+def getNumberOfEpisodes(link : str) -> int:
+    # Creates link to get data
+    link = f"https://gogoanimehd.to{link}"
+    # Creates new session
+    session : requests.Session = requests.session()
+    # Gets the page's source
+    htmlData = session.get(link).text
+    # Parses the data
+    soupedData = bs(htmlData, "lxml")
+    # Gets the container containing the data
+    container = soupedData.find("ul",{"id":"episode_page"})
+    # Gets the a tag with the episodes info
+    aTag = container.find("a",{"class":"active"})
+    # Returns the number of episodes as int
+    return int(aTag.get("ep_end"))
+
+# Function to make link to be used in getting episodes links
+def makeLink(linkCate : str) -> str:
+    # Gets the last part of the domain
+    name : str = linkCate.split("/")[-1]
+    # Returns the link
+    return f"https://gogoanimehd.to/{name}-episode"
+    
+# Starting function
+# Where all the magic happens
+def start():
+    # Gets the name from the user search
+    name : str = search()
+    # Gets the link
+    link : str = makeLink(name)
+    # Gets the number of episodes
+    numOfEpisodes : int = getNumberOfEpisodes(name)
+    print(f"There are {numOfEpisodes} episodes")
+    # Defines Number of Episodes
     try:
-        os.mkdir(f"./{name}")
+        startEp = int(input("Start from episode?: "))
+        numOfEps= int(input("How many episodes?: "))
+    except:
+        # Default values (1,1)
+        print("Error occured, defaulted values to (1,1)")
+        startEp = 1
+        numOfEps = 1
+    # Range of startEp to startEp and numOfEps exclusive
+    eps = list(range(startEp,startEp+numOfEps))
+    # Prompts the user to enter quality
+    try:
+        quality = input("Enter quality 360,480,720: ")
+    except:
+        # In case of exception we default it at 480
+        print("Defaulted at quality 480")
+        quality = 480
+    # Make the folder for the videos
+    try:
+        os.mkdir(f"./{name.split('/')[-1]}")
     except:
         print("Directory exists")
-    name = f"./{name}"
-except:
-    # Default value
-    print("Error occured, link is now default value.")
-    link = "https://gogoanime.hu//k-on-2-episode"
-    name = "Generic"
-# Takes email input
-email = input("Enter your email: ")
-# Takes password input
-password = input("Enter your password: ")
+    email = input("Enter your email: ")
+    # Takes password input
+    password = input("Enter your password: ")
 
+    name = f"./{name.split('/')[-1]}"
+    DownloadTheFiles(LoginAndGoToLink(eps,email,password,link,quality),name,startEp,link,quality)
+    
 # This function logs in and fetches each download link indiviually
-def LoginAndGoToLink(eps,Email,Password,LinkofPath):
+def LoginAndGoToLink(eps,email,password,LinkofPath,quality):
     # Array of links to be downloaded
     Links = []
     # Login page link
     
-    linkLogin = "https://gogoanime.hu/login.html"
+    linkLogin = "https://gogoanimehd.to/login.html"
 
     # Requests session to start handshake
     s = requests.session()
@@ -56,7 +149,7 @@ def LoginAndGoToLink(eps,Email,Password,LinkofPath):
             csrftoken = i.get("content")
 
     # Dictionary for storing user data add your email and password 
-    login_data = dict(email=Email, password=Password, _csrf=csrftoken, next='/')
+    login_data = dict(email=email, password=password, _csrf=csrftoken, next='/')
 
     # Logs in using the data
     s.post(linkLogin,data=login_data, headers=dict(Referer=linkLogin))
@@ -73,21 +166,12 @@ def LoginAndGoToLink(eps,Email,Password,LinkofPath):
         # Finds download links
         for link in soup.find_all("a"):
             # You can set the resloution from '360, 480, 720, 1080'
-            if "480" in format(link.text):
+            if quality in format(link.text):
                 x = link.get("href")
                 Links.append(x) 
     # Returns Links array to other functions
     return Links
 
-# Not used anymore
-# Downloading function, takes Links array as a parameter
-def WGetTheFiles(Links):
-    name = startEp
-    for Link in Links:
-        # Downloads each file
-        os.system(f"wget -O EP{name}.mp4 {Link}")
-        name+=1
-# Generates random color code for progress bar
 def generate_hex_color_code():
     # Generate a random 6-digit hex number
     hex_num = ''.join(random.choices(string.hexdigits, k=6))
@@ -96,32 +180,45 @@ def generate_hex_color_code():
     return color_code
 
 # Used to download the file and is multiplatform
-def DownloadTheFiles(Links,path):
+def DownloadTheFiles(Links,path,startEp,mainLink,quality):
     name = startEp
     for link in Links:
-        # Get the file in chunks
-        response = requests.get(link, stream=True)
-        # Calculate size in Bytes
-        total_size_in_bytes = int(response.headers.get('content-length', 0))
-        # Block size of progress bar
-        block_size = 1024  # 1 Kibibyte
-        # Actual progress bar
-        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True,colour=generate_hex_color_code(),desc=f"EP{name}.mp4 ")
-        # Opens file in binary write mode
-        with open(f"{path}/EP{name}.mp4", "wb") as file:
-            # Loops through response as it write it in chunks
-            for data in response.iter_content(block_size):
-                # Updates progress bar
-                progress_bar.update(len(data))
-                # Writes data
-                file.write(data)
-        # Closes progress bar to avoid memory leaks
-        progress_bar.close()
+        max_attempts = 3
+        for attempt in range(max_attempts+1):
+            # Get the file in chunks
+            response = requests.get(link, stream=True)
+            if response.status_code == 200:
+                # Calculate size in Bytes
+                total_size_in_bytes = int(response.headers.get('content-length', 0))
+                # Block size of progress bar
+                block_size = 1024  # 1 Kibibyte
+                # Actual progress bar
+                progress_bar = tqdm(total=total_size_in_bytes, unit='KB', unit_scale=True,colour=generate_hex_color_code(),desc=f"EP{name}.mp4 ")
+                # Opens file in binary write mode
+                with open(f"{path}/EP{name}.mp4", "wb") as file:
+                    # Loops through response as it write it in chunks
+                    for data in response.iter_content(block_size):
+                        # Updates progress bar
+                        progress_bar.update(len(data))
+                        # Writes data
+                        file.write(data)
+                # Closes progress bar to avoid memory leaks
+                progress_bar.close()
+                break
+            else:
+                # If the second attempt fails we download it in another quality
+                if attempt == 2:
+                    if quality == "360":
+                        otherQuality = "480"
+                    else:
+                        otherQuality = "360"
+                    link = LoginAndGoToLink(list(range(name,name+1)),mainLink,otherQuality)[0]
+                print("Attempt failed, retrying....")
+                time.sleep(3)
         # Increments name
         name+=1
 
 # WGetTheFiles(LoginAndGoToLink(eps,link),name)
-DownloadTheFiles(LoginAndGoToLink(eps,email,password,link),name)
-
-
+if __name__ == '__main__':
+    start()
 # ENJOY
